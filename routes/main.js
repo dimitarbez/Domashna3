e8f148a1498fdcc496ee9e6eededf628b4c62831
Dimitar Bezhanovski <dimitarbez@gmail.com>
2021-12-27 21:58:28 +0100

    setup

--- routes/main.js
+++ routes/main.js
@@ -16,37 +16,25 @@ router.get('/history', middleware.isLoggedIn, (req, res) => {
     User.findById(req.user._id)
         .populate('history')
         .exec((err, user) => {
-            if(err) { 
-                req.flash('error', 'Username or password do not match!')
-                return res.redirect('/login')
-            }
             res.render('history', {gateways: user.history})
         })
 })
 
 
 router.get('/map', middleware.isLoggedIn, (req, res) => {
-    // LoraGateway.find({}, (err, gateways) => {
-    //     if (err) {
-    //         console.log(err)
-    //     } else {
-    //         //gateways = gateways.slice(0, 200)
-    //         console.log(gateways)
-    //     }
-    // })
-    res.render('map')
+    LoraGateway.find({}, (err, gateways) => {
+        if (err) {
+            console.log(err)
+        } else {
+            gateways = gateways.slice(0, 200)
+            console.log(gateways)
+            res.render('map', {gateways: JSON.stringify(gateways)})
+        }
+    })
 })
 
 
 router.get('/login', (req, res) => {
-    res.render('login')
-})
-
-
-// login hangle
-router.post('/login', (req, res, next) => {
-
-    passport.authenticate('res) => {
     res.render('login')
 })
 
@@ -122,7 +110,7 @@ router.post('/register', (req, res) => {
 })
 
 
-router.get('/submit_gateway', middleware.isLoggedIn, (req, res) => {
+router.get('/submit_gateway', (req, res) => {
     res.render('submit_gateway')
 })
 
